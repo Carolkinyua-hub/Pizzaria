75e42f324cb8653e9b6807d9c05d11c11201413e
Carolkinyua-hub <carolnkinyua@gmail.com>
2021-11-02 16:05:32 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,17 @@
-let large=
-let 
+$(document).getready(function(submit){
+
+function order(size,crust,toppings,number){ 
+	this.size=size;
+	this.toppings=toppings;
+	this.number=number;
+	this.crust=crust;
+
+function small(size,crust,topping,small){
+let sc=$("#crust").val();
+let st=$("#toppings").val();
+let ss=$("#small").val();
+
+};
+});
+
+
