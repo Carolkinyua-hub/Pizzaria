91ad124560ca48b6a92778b6dc73c461c012218b
Carolkinyua-hub <carolnkinyua@gmail.com>
2021-11-03 15:18:21 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,2 @@
 $(document).getready(function(submit){
-
-function order(size,crust,toppings,number){ 
-	this.size=size;
-	this.toppings=toppings;
-	this.number=number;
-	this.crust=crust;
-
-function small(crust,topping){
-let sc=$("#crust").val();
-let st=$("#toppings").val();
-let st=$("#location").val();
- 
-function smallCost(sc, st){
-	let sc=500;
-	let st=200
-	let pizzaCost=(sc+st);
-};
-};
-};
 });
-
-
