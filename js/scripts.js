a0ba226d29f621a4b35e13772d06d6d439f1425c
Carolkinyua-hub <carolnkinyua@gmail.com>
2021-11-04 20:14:26 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,21 @@ $(document).ready(function(){
 $("#submit").click(function(){
 let sizes=$("#size").val();
 console.log(sizes)
+let pizzaS=$("#size option:selected").text();
+console.log(pizzaS)
 let crusts=$("#crust").val();
 console.log(crusts)
+let pizzaC=$("#crust option:selected").text();
 let toppings=$("#topping").val();
 console.log(toppings)
+let pizzaT=$("#topping option:selected").text();
 let Total= parseInt(toppings)+ parseInt(crusts) +parseInt(sizes);
 console.log(Total);
-$("#totals").append(Total);
+$(".sz").append(pizzaS);
+$(".cr").append(pizzaC);
+$(".tp").append(pizzaT);
+$(".totals").append(Total);
+
 
 
 
