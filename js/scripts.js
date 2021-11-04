a29cb418fe12178f5a542b909e5149e3f55cfbe2
Carolkinyua-hub <carolnkinyua@gmail.com>
2021-11-04 19:33:31 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
-$(document).getready(function(submit){
-	$("#size").val();
-	$("#crust").val();
-	$("#toppings").val();
+$(document).ready(function(){
+$("#submit").click(function(){
+let sizes=$("#size").val();
+console.log(sizes)
+let crusts=$("#crust").val();
+console.log(crusts)
+let toppings=$("#topping").val();
+console.log(toppings)
+let Total= parseInt(toppings)+ parseInt(crusts) +parseInt(sizes);
+console.log(Total);
+$("#totals").append(Total);
+
+
+
+event.preventDefault();
+	});
+
+
 });
