277e5efee48f095d21bfd85af67534b538e5a0da
Carolkinyua-hub <carolnkinyua@gmail.com>
2021-11-04 20:26:02 +0300

    add changes

--- js/scripts.js
+++ js/scripts.js
@@ -12,9 +12,9 @@ console.log(toppings)
 let pizzaT=$("#topping option:selected").text();
 let Total= parseInt(toppings)+ parseInt(crusts) +parseInt(sizes);
 console.log(Total);
-$(".sz").append(pizzaS);
-$(".cr").append(pizzaC);
-$(".tp").append(pizzaT);
+$(".sz").append(" "+ pizzaS +" "+sizes );
+$(".cr").append(" "+ pizzaC + ''+ crusts );
+$(".tp").append(" "+ pizzaT + "" + toppings);
 $(".totals").append(Total);
 
 
