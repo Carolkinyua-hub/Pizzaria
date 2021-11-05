5f872a64358b28cf79ec7eae0b558643490ce4fa
Carolkinyua-hub <carolnkinyua@gmail.com>
2021-11-05 15:10:24 +0300

    add code

---
+++ README.md
@@ -0,0 +1,22 @@
+# {J'z Pizzaria}
+#### {https://carolkinyua-hub.github.io/Pizzaria}, {2021}
+#### By **{Caroline Kinyua}**
+## Description
+{This website allows you to order a pizza
+}
+## Setup/Installation Requirements
+* On GitHub, navigate to the main page of the repository.
+* To clone the repository using HTTPS, under "Clone with HTTPS".
+* Open Terminal.Change the current working directory to the location where you want the cloned directory.
+* Type git clone, and then paste the URL you copied earlier.
+$ git clone https://github.com/Carolkinyua-hub/My-Work.git
+
+## Known Bugs
+Doing multiple purchases
+## Technologies Used
+{HTML ,Javascript, and CSS using the Bootstrap framework
+## Support and contact details
+Feel free to fork and make changes to the code
+### License
+*{[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)}*
+Copyright (c) {2021} **{Caroline Kinyua}**
--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,39 @@
 $(document).ready(function(){
 
-$("#submit").click(function(){
-let sizes=$("#size").val();
-console.log(sizes)
-let pizzaS=$("#size option:selected").text();
-console.log(pizzaS)
-let crusts=$("#crust").val();
-console.log(crusts)
-let pizzaC=$("#crust option:selected").text();
-let toppings=$("#topping").val();
-console.log(toppings)
-let pizzaT=$("#topping option:selected").text();
-let Total= parseInt(toppings)+ parseInt(crusts) +parseInt(sizes);
-console.log(Total);
-$(".sz").append(" "+ pizzaS + " "+" " + "   "+ "   "+ " "+" KSH "+ " " + "   "+ "   "+  sizes );
-$(".cr").append(" "+ pizzaC + " "+" " + "   "+ "   "+ " "+' KSH '+ " " + "   "+ "   " + "   "+   crusts );
-$(".tp").append(" "+ pizzaT + " "+" " + "   "+ "   "+" " +" KSH " + " " + "   "+ "   "+ "   "+  toppings);
-$("#totals").append("KSH" +" "+Total);
+	$("#submit").click(function(){
 
+	let sizes=$("#size").val();
+	console.log(sizes)
+	let crusts=$("#crust").val();
+	console.log(crusts)
+	let toppings=$("#topping").val();
+	console.log(toppings)	;
+	let Total=parseInt(sizes)+parseInt(crusts)+parseInt(toppings)
+	console.log(Total);
+	let sizez=$("#size option:selected").text();
+	console.log(sizez)
+	let crustz=$("#crust option:selected ").text();
+	console.log(crustz)
+	let toppingz=$("#topping option:selected ").text();
+	console.log(toppingz)	
+	let pizzas=[sizes,crusts,toppings,Total]
+	let pizzaz=[sizez,crustz,toppingz]
+	console.log(pizzas)
+	
+	$(".Order1").show();
+	$(".sz1").append(sizes)
+	$(".cr1").append(crusts)
+	$(".tp1").append(toppings)
+	$("#sz11").append(" "+ sizez +"Pizza" + ":")
+	$("#cr11").append(" "+ crustz + "Crust" +":")
+	$("#tp11").append(" "+ toppingz +"Toppings" +":")
+	$("#totals").append(Total)
+	event.preventDefault();	
 
-
-
-
-event.preventDefault();
-	});
-
-
+})
+			
+	
 });
+
+
+
